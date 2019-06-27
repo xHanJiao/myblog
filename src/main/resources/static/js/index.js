0d9b2700165c43fb98aa1ee647e80b6d5adf1984
韩潇 <623068955@qq.com>
2019-06-27 12:03:35 +0800

    把collapsible的js移到了单独的js文件中，还微调了首页的样式

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,10 +1,3 @@
-function triggerLi(idx) {
-    $('.collapsible-header').each(function (index) {
-        if (index === idx) {
-            $(this).trigger('click');
-        }
-    });
-}
 
 $(document).ready(function () {
 
@@ -44,46 +37,5 @@ $(document).ready(function () {
         });
     });
 
-    var $collapsible = $('.collapsible');
-
-    $collapsible.collapsible({
-        onOpen: function (el) {
-            $('.createTimeHolder').hide();
-            $(el).find('span[class *= createTimeHolder]').show();
-            var sub = initIdx - $(el).index();
-            while (sub !== 0) {
-                var endOne = $('.titleLi:last'),
-                    startOne = $('.titleLi:first');
-                if (sub > 0) {
-                    startOne.before(endOne);
-                    sub -= 1;
-                } else {
-                    endOne.after(startOne);
-                    sub += 1;
-                }
-            }
-        }
-    });
-
-    var $actDiv = $('.collapsible-header'),
-        totalLiNum = $actDiv.length,
-        initIdx = Math.floor(totalLiNum / 2);
-    triggerLi(initIdx);
-
-    $actDiv.addClass(bodyColor);
-
-    $collapsible.on('mousewheel', function (event) {
-        $('.createTimeHolder').hide();
-        var endOne = $('.titleLi:last'),
-            startOne = $('.titleLi:first');
-        if (event.deltaY < 0) {
-            startOne.before(endOne);
-        } else if (event.deltaY > 0) {
-            endOne.after(startOne);
-        } else {
-
-        }
-        triggerLi(initIdx);
-    });
 
 });
